26515318d12841faa6359495fd580a83fba4f35f
chocoscoding <chocoscoding@gmail.com>
2024-02-19 10:26:00 -0500

    project v1

---
+++ next.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+
+      {
+        protocol: 'https',
+        hostname: "lh3.googleusercontent.com",
+        port: '',
+        pathname: '/**',
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ];
+  },
+
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
